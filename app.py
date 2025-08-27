62f9bb29f97418c6bf99bda0cd8934f6de968f02
Anas-Tawalbeh <59394028+Anas-Tawalbeh@users.noreply.github.com>
2025-08-27 10:48:35 +0700

    “update

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Salam!</h1>'
+    user_agent = request.headers.get('User-Agent')
+    return f"""
+        <h1>Salam!</h1>
+        <p>Your browser is: {user_agent}</p>
+    """
 
-
+@app.route('/user/<name>')
+def user(name):
+    return f"<h1>Salam, {name}!</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)

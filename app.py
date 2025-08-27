2e5a40e1cb998a4d2d3b20511203f6711329363e
Anas-Tawalbeh <59394028+Anas-Tawalbeh@users.noreply.github.com>
2025-08-27 11:03:46 +0700

    “update

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect, abort
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent')
-    return f"""
+    response = make_response(f"""
         <h1>Salam!</h1>
         <p>Your browser is: {user_agent}</p>
-    """
+        <p>Document with cookie!</p>
+    """)
+    response.set_cookie('answer', '42')
+    return response
 
 @app.route('/user/<name>')
 def user(name):
     return f"<h1>Salam, {name}!</h1>"
+
+@app.route('/redirect')
+def go():
+    return redirect('http://www.example.com')
+
+@app.route('/user_id/<id>')
+def get_user(id):
+    if id not in ['1', '2']:
+        abort(404)
+    name = 'Anas' if id == '1' else 'Rema'
+    return f"<h1>Hello, {name}</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)

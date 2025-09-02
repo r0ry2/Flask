2621993a22eaa31acb672f1518a07c7cf0f1ccd6
Anas-Tawalbeh <59394028+Anas-Tawalbeh@users.noreply.github.com>
2025-09-02 16:40:10 +0700

    “update

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_bootstrap import Bootstrap  # ✔ صححنا الاستيراد
 app = Flask(__name__)
 bootstrap = Bootstrap(app)  # تهيئة Flask-Bootstrap  
 
-# الصفحة الرئيسية: تعرض user-agent + تضيف cookie + تعرض قالب index.html
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent')
@@ -12,7 +11,6 @@ def index():
     response.set_cookie('answer', '42')
     return response
 
-# صفحة مستخدم ديناميكية مع متغيرات وفلاتر
 @app.route('/user/<name>')
 def user_page(name):
     mydict = {'fruit': 'Apple'}
@@ -46,6 +44,15 @@ def get_user(id):
         abort(404)
     name = 'Anas' if id == '1' else 'Rema'
     return f"<h1>Hello, {name}</h1>"
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
